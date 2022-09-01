f4d18616debee7a08516b3d0f361dd91fb3322b3
Amine Ben Jemia <abenjemi@purdue.edu>
2022-09-01 18:20:44 -0400

    adding files to local repo

---
+++ WarGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarGame
+//
+//  Created by Amine Ben Jemia on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Amine Ben Jemia on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
